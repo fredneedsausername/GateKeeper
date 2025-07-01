64a5787786d550630466883f07ff88cbf82628f3
fred ver <fred.ver06@gmail.com>
2025-07-01 05:20:35 +0200

    Radical rework of project structure

--- frontend/src/components/layout/MainContent.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  // No props needed for this simple wrapper component
-</script>
-
-<main class="flex-1 overflow-auto bg-gray-50">
-  <div class="h-full">
-    <slot></slot>
-  </div>
-</main>
